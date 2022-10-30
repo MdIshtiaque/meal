e6245ecafe318f0a30526147fa564f02850d003c
MdIshtiaque <emon123653@gmail.com>
2022-10-30 22:27:17 +0600

    pass val fix

--- register1.php
+++ register1.php
@@ -1,35 +1,31 @@
 <?php
 
-// if (isset($_POST['username'])) {
-//     $server = "localhost";
-//     $username = "root";
-//     $password = "";
+if (isset($_POST['username'])) {
+    $server = "localhost";
+    $username = "root";
+    $password = "";
 
-//     $con = mysqli_connect($server, $username, $password);
+    $con = mysqli_connect($server, $username, $password);
 
-//     if (!$con) {
-//         die("connection to this database failed due to" .
-//             mysqli_connect_error());
-//     }
+    if (!$con) {
+        die("connection to this database failed due to" .
+            mysqli_connect_error());
+    }
 
-//     $username = $_POST['username'];
-//     $email = $_POST['email'];
-//     $m_no = $_POST['m_no'];
-//     $password = $_POST['pass'];
-//     $repass = $_POST['re_pass'];
-
-//     if ($password == $repass) {
-//         $sql = "INSERT INTO `test db`.`user_detailes` (`username`, `email`, `m_no`,
-//             `password`) VALUES ('$username', '$email',
-//             '$m_no', '$password')";
-//         mysqli_query($con, $sql);
-//     } else {
-//         echo "<p>Password not matching</p>";
-//     }
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $m_no = $_POST['m_no'];
+    $password = $_POST['pass'];
+    $repass = $_POST['re_pass'];
+    echo "Hi";
+    $sql = "INSERT INTO `test db`.`user_detailes` (`username`, `email`, `m_no`,
+            `password`) VALUES ('$username', '$email',
+            '$m_no', '$password')";
+    mysqli_query($con, $sql);
 
 
-//     $con->close();
-// }
+    $con->close();
+}
 
 ?>
 
@@ -51,17 +47,17 @@
         <p>Enter your informations</p>
 
         <form action="register1.php" class="form" method="post" autocomplete="off" id="form">
-            <input type="text" name="username" id="username" placeholder="Enter Your Home Username" required autocomplete="off">
-            <input type="email" name="email" id="email" placeholder="Enter Your Email" required autocomplete="off">
-            <input type="number" name="m_no" id="m_no" placeholder="Enter member number" required autocomplete="off">
-            <input type="password" name="pass" id="pass" placeholder="Enter a password" required autocomplete="off" onkeyup='btnn()'>
-            <input type="password" name="re_pass" id="re_pass" placeholder="Re-enter Your Password" onkeyup='btnn()' required autocomplete="off">
+            <input type="text" name="hidden" id="username" placeholder="Enter Your Home Username" required autocomplete="false">
+            <input type="email" name="hidden" id="email" placeholder="Enter Your Email" required autocomplete="false">
+            <input type="number" name="hidden" id="m_no" placeholder="Enter member number" required autocomplete="false">
+            <input type="password" name="hidden" id="pass" placeholder="Enter a password" required autocomplete="new-password" onkeyup='btnn()'>
+            <input type="password" name="hidden" id="re_pass" placeholder="Re-enter Your Password" onkeyup='btnn()' required autocomplete="false">
             <span id="error"></span>
-            <button type="submit" value="Next" id="btn"  >Done</button>
+            <input type="submit"  name="Submit" id="Submit" value="Done" id="btn">
             <!-- <button class="btn" id="btn" onclick="btn()">Next</button> -->
         </form>
-            
-        
+
+
 
     </div>
 </body>
@@ -76,18 +72,19 @@
 </html>
 
 <script>
-    function btnn(){
+    function btnn() {
         event.preventDefault()
         console.log("hi")
         var pass = document.getElementById("pass").value;
         var re_pass = document.getElementById("re_pass").value;
-        if(pass != re_pass){
+        if (pass != re_pass) {
             document.getElementById("error").hidden = false;
             document.getElementById("error").innerHTML = "Password doesn't matching!";
             document.getElementById("error").style.color = "Red";
-        }else{
+            document.getElementById("btn").disabled = true;
+        } else {
             document.getElementById("error").hidden = true;
+            document.getElementById("btn").disabled = false;
         }
     }
-  
 </script>
